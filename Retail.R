d83a8b492a89fab9f25236dbefe7dbbad2c7992b
mlsediaz <lsdiazcano@gmail.com>
2018-06-06 11:15:44 +0200

    Created worldmap function
    I have created a function to plot the corresponding world map

--- Retail.R
+++ Retail.R
@@ -1,5 +1,28 @@
 setwd("/Users/sediaz/Documents/Ubiqum/Proyectos alumnos/Mikel")
 
+#### 0. Creating my own functions ####
+
+
+createWorldMap <- function(my_table, my_title){
+# Must contain a column called Country  
+  ddf <- as.data.frame(table(my_table$Country))
+  names(ddf) <- c("country", "value")
+  ddf$value <- log(ddf$value)
+  spdf <- joinCountryData2Map(ddf, joinCode="NAME", nameJoinColumn="country")
+  my_world_map <- mapCountryData(spdf, nameColumnToPlot="value", 
+                 colourPalette = "white2Black", 
+                 borderCol = "white",
+                 addLegend = TRUE,
+                 oceanCol = "steelblue3",
+                 catMethod="fixedWidth",
+                 missingCountryCol = "navajowhite3",
+                 mapTitle = my_title)
+  
+  return(my_world_map)
+  
+}
+
+
 #### 1. Reading Libraries ####
 library(caret)
 library(readxl)
@@ -13,13 +36,11 @@ head(onlineretail)
 
 
 # Plotting world map
-ddf <- as.data.frame(table(onlineretail$Country))
-names(ddf) <- c("country", "value")
-ddf$value <- log(ddf$value)
-spdf <- joinCountryData2Map(ddf, joinCode="NAME", nameJoinColumn="country")
-mapCountryData(spdf, nameColumnToPlot="value", 
-               catMethod="fixedWidth", 
-               mapTitle = "Countries with more purchases")
+
+createWorldMap(onlineretail, "Countries with more purchases")
+
+
+
 
 
 
